1959c6ff7996eefca3fee312d91146333fcd84c4
Selim Kazancı <selimkznci@users.noreply.github.com>
2021-02-10 03:15:06 +0300

    Generics MyDictionary

---
+++ GenericsIntro/GenericsIntro/MyDictionary1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    internal class MyDictionary<T1, T2>
+    {
+        T1[] keysArray;
+        T2[] valuesArray;
+        public MyDictionary()
+        {
+            keysArray = new T1[0];
+            valuesArray = new T2[0];
+        }
+        public void Add(T1 key, T2 value)
+        {
+            T1[] tempKey = keysArray;
+            T2[] tempValue = valuesArray;
+            keysArray = new T1[keysArray.Length + 1];
+            valuesArray = new T2[valuesArray.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keysArray[i] = tempKey[i];
+                valuesArray[i] = tempValue[i];
+            }
+            keysArray[keysArray.Length - 1] = key;
+            valuesArray[valuesArray.Length - 1] = value;
+        }
+        public void Values(int key)
+        {
+            for (int i = 0; i < keysArray.Length; i++)
+            {
+                if (Convert.ToInt32(keysArray[i]) == key)
+                {
+                    Console.WriteLine(valuesArray[i]);
+                }
+            }
+        }
+    }
+}
---
+++ GenericsIntro/GenericsIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> names = new MyDictionary<int, string>();
+            names.Add(1, "Selim");
+            names.Add(10, "Sueda");
+            names.Add(24, "Çağlar");
+            names.Add(12, "Mustafa");
+            names.Add(101, "Gökçe");
+            names.Add(999, "Altay");
+
+            names.Values(999);
+            Console.ReadLine();
+        }
+    }
+}
